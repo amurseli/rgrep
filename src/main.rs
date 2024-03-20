fa663d20cca513d2d8b10214726fe167fa20dca1
amurseli <amurseli@fi.uba.ar>
2024-03-20 17:12:38 +0100

    reading args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env::args;
+
+
+
 fn main() {
-    println!("Hello, github!");
+    let args: Vec<String> = args().collect();
+
+    let regex: &String = &args[1];
+    let file_path: &String = &args[2];
+    
+    println!("Matching {:?} expression inside {:?}...",regex,file_path);
+
+
 }
