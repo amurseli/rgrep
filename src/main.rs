5914efb71f482c8fc252a9ed78c446d495a3e662
amurseli <amurseli@fi.uba.ar>
2024-04-02 22:41:09 +0200

    Revamp y cambios

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,60 @@
-use std::env::args;
 use rgrep::regex::Regex;
-
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
-    //let args: Vec<&str> = args().collect();
+    // Obtener los argumentos de la línea de comandos
+    let args: Vec<String> = env::args().collect();
 
-    let pattern = Regex::new("ab[[:digit:]].e");
-    let file_path: &str = "absre";
-    
-    //println!("Matching {:?} expression inside {:?}...", pattern, file_path);
-
-    match pattern.unwrap().test(file_path) {
-        Ok(result) => println!("Result: {}", result),
-        Err(err) => println!("Error: {}", err),
+    // Verificar si se proporcionaron suficientes argumentos
+    if args.len() < 3 {
+        println!("Usage: {} <regex> <filepath>", args[0]);
+        return;
     }
 
+    // Obtener el patrón de expresión regular y la ruta del archivo desde los argumentos
+    let regex_str = &args[1];
+    let file_path = &args[2];
+
+    // Compilar el patrón de expresión regular
+    let pattern = match Regex::new(regex_str) {
+        Ok(pattern) => pattern,
+        Err(err) => {
+            println!("Error al crear la expresión regular: {}", err);
+            return;
+        }
+    };
+
+    // Intentar abrir el archivo
+    let file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("Error al abrir el archivo '{}': {}", file_path, err);
+            return;
+        }
+    };
+
+    // Crear un lector de líneas para el archivo
+    let reader = io::BufReader::new(file);
+
+    // Leer todas las líneas del archivo en un vector
+    let lines: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+
+    // Procesar cada línea para encontrar coincidencias con el patrón
+    for line in lines {
+        println!("La línea analizada es {:?}", line);
+        let mut pattern = match Regex::new(regex_str) {
+            Ok(regex) => regex,
+            Err(err) => {
+                println!("Error creating regex pattern: {}", err);
+                continue; // Saltar a la siguiente iteración si hay un error
+            }
+        };
+        match pattern.test(&line) {
+            Ok(result) => println!("{}", result),
+            Err(err) => println!("Error applying the regular expression pattern: {}", err),
+        }
+    }
 }
--- src/regex.rs
+++ src/regex.rs
@@ -1,6 +1,6 @@
 use std::collections::VecDeque;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Class {
     Alnum,
     Alpha,
@@ -11,34 +11,30 @@ enum Class {
     Punct,
 }
 
-struct EvaluatedStep{
-    step:RegexStep,
+struct EvaluatedStep {
+    step: RegexStep,
     size: usize,
-    backtrackeable: bool
-    
+    backtrackeable: bool,
 }
 
-#[derive(Debug)]
-enum RegexVal{
+#[derive(Debug, Clone)]
+enum RegexVal {
     Literal(char),
     Wildcard,
     Bracket(Vec<char>),
     NegatedBracket(Vec<char>),
     Class(Class),
-
 }
 
-impl RegexVal{
-
+impl RegexVal {
     fn match_class(value: char, class: &Class) -> usize {
         match class {
             Class::Alnum => {
                 if value.is_alphanumeric() {
-                    println!("El caracter {:?} coincidio",value);
+                    //println!("El caracter {:?} coincidio",value);
                     value.len_utf8()
                 } else {
-
-                    println!("El caracter {:?} NO coincidio",value);
+                    //println!("El caracter {:?} NO coincidio",value);
                     0
                 }
             }
@@ -84,52 +80,66 @@ impl RegexVal{
                     0
                 }
             }
-            _ => 0, 
+            _ => 0,
         }
     }
 
-
-    pub fn matches (&self, value:&str) -> usize {
+    pub fn matches(&self, value: &str) -> usize {
         match self {
             RegexVal::Literal(l) => {
-                if value.chars().next() == Some(*l){
-                    println!("El caracter {:?} coincidio",l);
+                if value.chars().next() == Some(*l) {
+                    println!("El caracter {:?} coincidio", l);
                     l.len_utf8()
-                } else{
-                    println!("El caracter {:?} NO coincidio con {:?}",l, value.chars().next());
+                } else {
+                    println!(
+                        "El caracter {:?} NO coincidio con {:?}",
+                        l,
+                        value.chars().next()
+                    );
                     0
                 }
-            },
+            }
             RegexVal::Wildcard => {
-                if let Some(w) = value.chars().next(){
+                if let Some(w) = value.chars().next() {
+                    println!("El caracter del texto {:?} coincidio con una wildcard", w);
                     w.len_utf8()
-                } else{
+                } else {
                     0
                 }
-            },
+            }
             RegexVal::Bracket(chars) => {
                 if let Some(c) = value.chars().next() {
                     if chars.contains(&c) {
+                        println!("El caracter {:?} se encontró en la Bracket", c);
                         c.len_utf8()
                     } else {
+                        println!("El caracter {:?} No se encontró en la Bracket", c);
                         0
                     }
                 } else {
                     0
                 }
-            },
+            }
             RegexVal::NegatedBracket(chars) => {
                 if let Some(c) = value.chars().next() {
                     if chars.contains(&c) {
+                        println!(
+                            "El caracter {:?} se encontró en la Bracket (lo cual es malo)",
+                            c
+                        );
                         0
                     } else {
+                        println!(
+                            "El caracter {:?} No se encontró en la Bracket (lo cual es bueno)",
+                            c
+                        );
                         c.len_utf8()
                     }
                 } else {
                     0
                 }
             }
-            RegexVal::Class(class) =>{
+            RegexVal::Class(class) => {
                 if let Some(c) = value.chars().next() {
                     Self::match_class(c, class)
                 } else {
@@ -140,54 +150,51 @@ impl RegexVal{
     }
 }
 
-#[derive(Debug)]
-pub struct RegexStep{
+#[derive(Debug, Clone)]
+pub struct RegexStep {
     val: RegexVal,
     rep: RegexRep,
 }
 
-#[derive(Debug)]
-enum RegexRep{
+#[derive(Debug, Clone)]
+enum RegexRep {
     Any,
     Exact(usize),
-    Range{
+    Range {
         min: Option<usize>,
         max: Option<usize>,
     },
-    //Bracket(Vec<char>),       
+    //Bracket(Vec<char>),
 }
 
-pub struct Regex{
-    steps: Vec<RegexStep>
+pub struct Regex {
+    steps: Vec<RegexStep>,
 }
 
-impl Regex{
-
-    pub fn handle_brackets(
-        char_iter: &mut std::str::Chars,
-    ) -> Result<RegexStep, &'static str> {
+impl Regex {
+    pub fn handle_brackets(char_iter: &mut std::str::Chars) -> Result<RegexStep, &'static str> {
         let mut chars = Vec::new();
         let mut negate = false;
         let mut closed = false;
         let mut is_class = false;
         let mut val = RegexVal::Literal(' ');
-        
+
         while let Some(ch) = char_iter.next() {
             match ch {
                 ']' => {
                     closed = true;
                     break;
-                },
+                }
                 '^' => negate = true,
                 '[' => {
-                    if(char_iter.next() == Some(':')){
+                    if (char_iter.next() == Some(':')) {
                         let mut class_name = String::new();
                         while let Some(name_ch) = char_iter.next() {
-                        if name_ch == ':' {
-                            is_class = true;
-                            break;
-                        }
-                        class_name.push(name_ch);
+                            if name_ch == ':' {
+                                is_class = true;
+                                break;
+                            }
+                            class_name.push(name_ch);
                         }
                         val = match class_name.as_str() {
                             "alnum" => RegexVal::Class(Class::Alnum),
@@ -201,11 +208,11 @@ impl Regex{
                         };
                         char_iter.next();
                     }
-                },
+                }
                 _ => chars.push(ch),
             }
         }
-        
+
         if !closed {
             return Err("No closing bracket found");
         }
@@ -223,94 +230,91 @@ impl Regex{
     }
 
     pub fn new(expression: &str) -> Result<Self, &str> {
-
         let mut steps: Vec<RegexStep> = Vec::new();
-        
+
         let mut char_iter = expression.chars();
-        while let Some(c) = char_iter.next(){
+        while let Some(c) = char_iter.next() {
             let step = match c {
-                '.' => Some(RegexStep{ 
-                    rep: RegexRep::Exact(1), 
-                    val: RegexVal::Wildcard 
+                '.' => Some(RegexStep {
+                    rep: RegexRep::Exact(1),
+                    val: RegexVal::Wildcard,
                 }),
-                'a'..='z' => Some(RegexStep{
-                    rep:RegexRep::Exact(1),
-                    val:RegexVal::Literal(c)
+                'a'..='z' => Some(RegexStep {
+                    rep: RegexRep::Exact(1),
+                    val: RegexVal::Literal(c),
                 }),
                 '*' => {
-                    if let Some(last) = steps.last_mut(){
+                    if let Some(last) = steps.last_mut() {
                         last.rep = RegexRep::Any;
-                    }else{
-                        return Err("'*' Inesperado")
+                    } else {
+                        return Err("'*' Inesperado");
                     }
                     None
                 }
-                '[' => {
-                    match Self::handle_brackets(&mut char_iter) {
-                        Ok(step) => Some(step),
-                        Err(err) => return Err(err),
-                    }
+                '[' => match Self::handle_brackets(&mut char_iter) {
+                    Ok(step) => Some(step),
+                    Err(err) => return Err(err),
                 },
-                ' ' => Some(RegexStep{
+                ' ' => Some(RegexStep {
                     rep: RegexRep::Exact(1),
-                    val: RegexVal::Literal(c)
+                    val: RegexVal::Literal(c),
                 }),
 
-                _ => return Err("Caracter Inesperado")
-
+                _ => return Err("Caracter Inesperado"),
             };
-            if let Some(p) = step{
+            if let Some(p) = step {
                 steps.push(p);
-
             }
         }
 
-        
-        
-        Ok(Regex{steps})
-        
+        Ok(Regex { steps })
     }
 
-    pub fn test(self, value: &str) -> Result<bool, &str>{
-        if !value.is_ascii(){
+    pub fn test(&mut self, value: &str) -> Result<bool, &str> {
+        if !value.is_ascii() {
             return Err("El input no es ascii");
         }
 
-        let mut queue = VecDeque::from(self.steps);
+        let mut queue = VecDeque::from(self.steps.clone());
+
         let mut stack: Vec<EvaluatedStep> = Vec::new();
         let mut index = 0;
-        'steps: while let Some(step) = queue.pop_front(){
+        'steps: while let Some(step) = queue.pop_front() {
             match step.rep {
                 RegexRep::Exact(n) => {
                     let mut match_size = 0;
-                    for _ in 0..=0{ //mirar este for
-                        println!("n {:?}", step.val);
-                        let size =  step.val.matches(&value[index..]);
-                        if size == 0{
-                            match backtrack(step, &mut stack, &mut queue){
-                                Some(size) =>{
+                    for _ in 0..=0 {
+                        //println!("n {:?}", step.val);
+                        let size = step.val.matches(&value[index..]);
+                        if size == 0 {
+                            match backtrack(&step, &mut stack, &mut queue) {
+                                Some(size) => {
                                     index -= size;
                                     continue 'steps;
                                 }
-                                None => return Ok(false),
+                                None => {
+                                    if (value.len() < index + 1) {
+                                        return Ok(false);
+                                    }
+                                    match_size += 1;
+                                    index += 1;
+                                }
                             }
-                        } else{
+                        } else {
                             match_size += size;
                             index += size;
-                            println!("Index {:?}", index)
+                            stack.push(EvaluatedStep {
+                                step: step.clone(),
+                                size: match_size,
+                                backtrackeable: false,
+                            });
                         }
                     }
-                    stack.push(EvaluatedStep{
-                        step: step,
-                        size: match_size,
-                        backtrackeable: false
-
-                    })
                 }
                 RegexRep::Any => {
                     let mut keep_matching = true;
                     println!("n {:?}", step.val);
-                    while keep_matching{        
+                    while keep_matching {
                         let match_size = step.val.matches(&value[index..]);
                         if match_size != 0 {
                             index += match_size;
@@ -323,12 +327,12 @@ impl Regex{
                                     }
                                 }
                             }
-                        }else{
+                        } else {
                             keep_matching = false;
                         }
                     }
-                },
-                RegexRep::Range{min, max} => todo!()
+                }
+                RegexRep::Range { min, max } => todo!(),
             }
         }
 
@@ -337,22 +341,21 @@ impl Regex{
 }
 
 fn backtrack(
-    current: RegexStep,
+    current: &RegexStep,
     evaluated: &mut Vec<EvaluatedStep>,
     next: &mut VecDeque<RegexStep>,
- ) -> Option<usize> {
+) -> Option<usize> {
     let mut back_size = 0;
-    next.push_front(current);
-    while let Some(e) = evaluated.pop(){
-    back_size += e.size;
-    if e.backtrackeable{
-        println!("Backtrack {:?}", back_size);
-        return Some(back_size);
-    } else {
-        next.push_front(e.step);
+    next.push_front(current.clone());
+    while let Some(e) = evaluated.pop() {
+        back_size += e.size;
+        if e.backtrackeable {
+            println!("Backtrack {:?}", back_size);
+            return Some(back_size);
+        } else {
+            next.push_front(e.step);
+        }
     }
-    }
-    
+
     None
 }
-
